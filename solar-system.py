0291dba4035c066575e8881887080778cfab385f
Ashwin <70658331+Ashwin-055@users.noreply.github.com>
2022-10-24 21:00:28 +0530

    Create solar-system.py

---
+++ solar-system.py
@@ -0,0 +1,13 @@
+import cv2
+img = cv2.imread("solar-system.jpg")
+cv2.putText(img,"Sun",(90,80),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255))
+cv2.putText(img,"Mercury",(110,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Venus",(185,170),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Earth",(285,265),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Mars",(380,173),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Jupiter",(580,378),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Saturn",(780,130),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Uranus",(965,290),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.putText(img,"Neptune",(1105,144),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,200,0))
+cv2.imshow("Solar System",img)
+cv2.waitKey(0)
